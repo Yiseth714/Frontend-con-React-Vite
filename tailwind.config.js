fc7383ea342b379ac01094d7fe3b6c50e22746f5
ItsKxteee <kate0212rodri@gmail.com>
2026-01-21 14:45:53 -0500

    Avance en el diccionario, numeros y palabras divididos por secciones

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,20 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,jsx}"
+//   ],
+//   theme: {
+//     extend: {
+//       colors: {
+//         primary: '#0B3C5D',
+//         secondary: '#4FA3D1',
+//         accent: '#F2C94C',
+//       },
+//     },
+//   },
+//   plugins: [],
+// }
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -10,6 +27,21 @@ export default {
         primary: '#0B3C5D',
         secondary: '#4FA3D1',
         accent: '#F2C94C',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': {
+            opacity: '0',
+            transform: 'scale(0.95)',
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'scale(1)',
+          },
+        },
+      },
+      animation: {
+        fadeIn: 'fadeIn 0.3s ease-out',
       },
     },
   },
