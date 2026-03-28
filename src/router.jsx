50a685718df400a0dee6e30798d43f3d3ca9da18
AlejandraCardonaJ <alejandra_jara@hotmail.com>
2026-03-28 11:39:56 -0500

    Confirmando cambio en router, para poder hacer nuevo merge.

--- src/router.jsx
+++ src/router.jsx
@@ -1,32 +1,3 @@
-
-// import React from 'react'
-
-// // Router
-// import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
-// // Pages
-// import Home from './pages/HomePages.jsx'
-// import Diccionario from './pages/Diccionario.jsx'
-// import Ruta from './pages/RutaAprendizaje/RutaHome.jsx'
-// import Traductor from './pages/Traductor.jsx'
-
-
-
-// function Router() {
-//   return (
-//     <BrowserRouter>
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/diccionario" element={<Diccionario />} />
-//         <Route path="/ruta" element={<Ruta />} />
-//         <Route path="/traductor" element={<Traductor />} />
-//       </Routes>
-//     </BrowserRouter>
-//   )
-// }
-
-// export default Router
-
 import React from 'react'
 
 // Router
