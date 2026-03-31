46d223cec800052bc582f22e64513042edb2cd56
Yiseth714 <yisethvaleguti@gmail.com>
2026-03-30 22:14:19 -0500

    Guardando proyecto funcional hasta la fecha

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import basicSsl from '@vitejs/plugin-basic-ssl'
+import fs from 'fs'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,7 +11,11 @@ export default defineConfig({
   ],
   server: {
     port: 5173,
-    host: '192.168.1.13',  // Acepta conexiones externas (el celular)
-    https: true,
+    host: '0.0.0.0',  // Acepta conexiones externas (el celular)
+    https: {                                    
+      key:  fs.readFileSync('../Backend-con-fastApi/certs/localhost+2-key.pem'),  
+      cert: fs.readFileSync('../Backend-con-fastApi/certs/localhost+2.pem'),      
+    }
+
   },
 })
